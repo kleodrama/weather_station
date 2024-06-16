15e8ed024831f64f0e626398bd94557d5dc78dc4
kleodrama <kleodrama@gmail.com>
2024-06-16 23:50:46 +0000

    first comit test in secrets

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import os
 import streamlit as st
+from supabase import create_client, Client
+
+
+url = st.secrets["supabase"]["url"]
+key = st.secrets["supabase"]["key"]
+
+supabase = create_client(url, key)
+
+response = supabase.table('roof_temperature').select("*").execute()
+
+st.header("Weather Station")
+
+
+st.write(response)
+
