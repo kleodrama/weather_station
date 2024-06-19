9540a35f4d6ad9aa9bafbea6280c37bc5189da78
kleo <kleodrama@gmail.com>
2024-06-20 02:52:34 +0300

    tab year

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import pandas as pd
 from datetime import datetime, timedelta
 import pytz
 
+import altair as alt
+
 # import locale
 
 # locale.setlocale(locale.LC_TIME, "el_GR")  # Greek
@@ -58,7 +60,9 @@ df = pd.DataFrame(data={
 })
 df['Ημερομηνία'] = df['Ημερομηνία/ώρα'].dt.strftime('%d %b %y')
 df['Ώρα'] = df['Ημερομηνία/ώρα'].dt.strftime('%H:%M')
-df['Μήνας/Έτος'] = df['Ημερομηνία/ώρα'].dt.strftime('%m/%y')
+df['Μήνας/Έτος'] = df['Ημερομηνία/ώρα'].dt.strftime('%m/%Y')
+df['Μήν/Έτος'] = df['Ημερομηνία/ώρα'].dt.strftime('%-m/%Y')
+df['Έτος'] = df['Ημερομηνία/ώρα'].dt.strftime('%Y')
 df['Ημέρα του μήνα'] = df['Ημερομηνία/ώρα'].dt.strftime('%d')
 
 today_temps = df.loc[df["Ημερομηνία"] == datetime.today().strftime('%d %b %y')]
@@ -80,10 +84,10 @@ with col1:
 with col2:
     ttt = today_max["Ημερομηνία/ώρα"].strftime('%H:%M')
     ddd = today_min["Ημερομηνία/ώρα"].strftime('%H:%M')
-    f'μέγιστη: **{today_max["Θερμοκρασία"]} °C** :clock1: :red-background[{ttt}]'
-    f'ελάχιστη: **{today_min["Θερμοκρασία"]} °C** :clock1: :blue-background[{ddd}]'
-    '---'
-    f'χθες ίδια ώρα: **{yesterday_same_time} °C**'
+    f'μέγιστη: :thermometer: **{today_max["Θερμοκρασία"]} °C** :clock1: :red-background[{ttt}]'
+    f'ελάχιστη: :thermometer: **{today_min["Θερμοκρασία"]} °C** :clock1: :blue-background[{ddd}]'
+    # st.markdown("***")
+    f'χθες ίδια ώρα: :thermometer: **{yesterday_same_time} °C**'
 
 '---'
 
@@ -131,29 +135,64 @@ with tab_day:
 
 
 def go_to_next_month():
-    global selected_month
-    selected_month = selected_month + timedelta(days=30)
+    if st.session_state['month'] < 12:
+        st.session_state['month'] = int(st.session_state['month']) + 1
+    else:
+        st.session_state['month'] = 1
+        st.session_state['year'] = int(st.session_state['year']) + 1
+
+
+def go_to_previous_month():
+    if st.session_state['month'] > 1:
+        st.session_state['month'] = int(st.session_state['month']) - 1
+    else:
+        st.session_state['month'] = 12
+        st.session_state['year'] = int(st.session_state['year']) - 1
 
 
 with tab_month:
-    '''Λίστα με όλες τις ημέρες του μήνα (μέγιστη, ελάχιστη, μέση)'''
-    try:
-        selected_month
-    except:
+    # '''Λίστα με όλες τις ημέρες του μήνα (μέγιστη, ελάχιστη, μέση)'''
+    if 'month' in st.session_state and 'year' in st.session_state:
+        month = st.session_state['month']
+        year = st.session_state['year']
+        selected_month = datetime.now().replace(year=year, month=month)
+    else:
         selected_month = datetime.now()
-    st.write(selected_month)
-    filtered_df_month = df.loc[(df["Μήνας/Έτος"] == selected_month.strftime('%m/%y'))]
+        st.session_state['month'] = datetime.now().month
+        st.session_state['year'] = datetime.now().year
+    filtered_df_month = df.loc[(df["Μήνας/Έτος"] == selected_month.strftime('%m/%Y'))]
     c_month_1, c_month_2, c_month_3 = st.columns(3)
     with c_month_2:
-        st.subheader(f'{datetime.now().strftime("%B")} {datetime.now().year}')
-        st.line_chart(filtered_df_month, x="Ημέρα του μήνα", y="Θερμοκρασία")
-        st.write(filtered_df_month)
+        st.subheader(f'{selected_month.strftime("%B")} {selected_month.year}')
     with c_month_1:
-        st.button("Προηγούμενος Μήνας")
+        st.button("Προηγούμενος Μήνας", on_click=go_to_previous_month)
     with c_month_3:
         st.button(label="Επόμενος Μήνας", on_click=go_to_next_month)
 
+    st.line_chart(filtered_df_month, x="Ημέρα του μήνα", y="Θερμοκρασία")
+    st.write(filtered_df_month)
 
+
+with tab_year:
+    # if 'yyear' in st.session_state:
+    #     yyear = st.session_state['yyear']
+    #     selected_year = datetime.now().replace(year=yyear)
+    # else:
+    #     selected_year = datetime.now()
+    #     st.session_state['yyear'] = datetime.now().year
+    option = st.selectbox("Επιλογή έτους", pd.unique(df["Έτος"]))
+    st.write("Επιλεγμένο έτος: ", option)
+    filtered_df_year = df.loc[df["Έτος"] == f'{option}']
+    for i in range(12):
+        filtered_df_year_per_month = df.loc[df["Μήν/Έτος"] == f'{i+1}/{option}']
+        f':sunny: Μήνας: {i+1}/{option}'
+        f':gray[**Μέση** :thermometer: {round(filtered_df_year_per_month["Θερμοκρασία"].mean(), 1)}] | :red[**Μέγιστη** :thermometer: {round(filtered_df_year_per_month["Θερμοκρασία"].max(), 1)}] | :blue[**Ελάχιστη** :thermometer: {round(filtered_df_year_per_month["Θερμοκρασία"].min(), 1)}]'
+        '---'
+    chart = alt.Chart(filtered_df_year).mark_boxplot(extent='min-max').encode(
+        x='Μήν/Έτος',
+        y='Θερμοκρασία'
+    )
+    st.altair_chart(chart, theme="streamlit", use_container_width=True)
 # st.write(data)
 
 st.image('https://lh3.googleusercontent.com/a/ACg8ocKf_BEQXNtWgvgzj2Jump2CMCB5dCo95RWSdQdsh_67Ga3prkYU=s288-c-no',
