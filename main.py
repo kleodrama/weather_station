514fc459856562b8b92bf5db3d24207ef9d63dfd
kleo <kleodrama@gmail.com>
2024-07-10 01:18:30 +0300

    Pressure and Humidity diagrams added

--- main.py
+++ main.py
@@ -115,6 +115,18 @@ df_pressure['Μήν/Έτος'] = df_pressure['Ημερομηνία/ώρα'].dt.s
 df_pressure['Έτος'] = df_pressure['Ημερομηνία/ώρα'].dt.strftime('%Y')
 df_pressure['Ημέρα του μήνα'] = df_pressure['Ημερομηνία/ώρα'].dt.strftime('%d')
 
+
+df_humidity = pd.DataFrame(data={
+    'Ημερομηνία/ώρα': list_h_datetimes,
+    'Υγρασία': list_humidities
+})
+df_humidity['Ημερομηνία'] = df_humidity['Ημερομηνία/ώρα'].dt.strftime('%d %b %y')
+df_humidity['Ώρα'] = df_humidity['Ημερομηνία/ώρα'].dt.strftime('%H:%M')
+df_humidity['Μήνας/Έτος'] = df_humidity['Ημερομηνία/ώρα'].dt.strftime('%m/%Y')
+df_humidity['Μήν/Έτος'] = df_humidity['Ημερομηνία/ώρα'].dt.strftime('%-m/%Y')
+df_humidity['Έτος'] = df_humidity['Ημερομηνία/ώρα'].dt.strftime('%Y')
+df_humidity['Ημέρα του μήνα'] = df_humidity['Ημερομηνία/ώρα'].dt.strftime('%d')
+
 today_temps = df.loc[df["Ημερομηνία"] == datetime.today().strftime('%d %b %y')]
 today_max = today_temps.loc[today_temps["Θερμοκρασία"].idxmax()]
 today_min = today_temps.loc[today_temps["Θερμοκρασία"].idxmin()]
@@ -173,6 +185,8 @@ with tab_day:
                                                                                next_day.strftime('%Y-%m/%d'))]
     filtered_df_pressure = df_pressure.loc[(df_pressure["Ημερομηνία/ώρα"] >= day.strftime('%Y-%m/%d')) &
                                            (df_pressure["Ημερομηνία/ώρα"] <= next_day.strftime('%Y-%m/%d'))]
+    filtered_df_humidity = df_humidity.loc[(df_humidity["Ημερομηνία/ώρα"] >= day.strftime('%Y-%m/%d')) &
+                                           (df_humidity["Ημερομηνία/ώρα"] <= next_day.strftime('%Y-%m/%d'))]
     if filtered_df.size > 0:
         c1, c2, c3 = st.columns(3)
         with c1:
@@ -202,6 +216,10 @@ with tab_day:
 
         st.altair_chart(c, use_container_width=True)
         # st.line_chart(filtered_df_pressure, y="Πίεση", x='Ώρα')
+
+    if filtered_df_humidity.size > 0:
+        st.line_chart(filtered_df_humidity, y="Υγρασία", x='Ώρα')
+
     if filtered_df.size > 0:
         st.dataframe(filtered_df
                     .style.highlight_max(axis=0, subset=['Θερμοκρασία'], props='background-color:red;')
@@ -216,6 +234,14 @@ with tab_day:
                     .highlight_min(axis=0, subset=['Πίεση'], props='background-color:blue;')
                      .format(precision=1),
                     column_order=["Ώρα", "Πίεση"],
+                    hide_index=True,
+                    use_container_width=True)
+    if filtered_df_humidity.size > 0:
+        st.dataframe(filtered_df_humidity
+                    .style.highlight_max(axis=0, subset=['Υγρασία'], props='background-color:red;')
+                    .highlight_min(axis=0, subset=['Υγρασία'], props='background-color:blue;')
+                     .format(precision=1),
+                    column_order=["Ώρα", "Υγρασία"],
                     hide_index=True,
                     use_container_width=True)
     else:
@@ -250,7 +276,11 @@ with tab_month:
         st.session_state['year'] = datetime.now().year
         month = st.session_state['month']
         year = st.session_state['year']
+
     filtered_df_month = df.loc[(df["Μήνας/Έτος"] == selected_month.strftime('%m/%Y'))]
+    filtered_df_p_month = df_pressure.loc[(df_pressure["Μήνας/Έτος"] == selected_month.strftime('%m/%Y'))]
+    filtered_df_h_month = df_humidity.loc[(df_humidity["Μήνας/Έτος"] == selected_month.strftime('%m/%Y'))]
+
     c_month_1, c_month_2, c_month_3 = st.columns(3)
     with c_month_2:
         st.subheader(f'{selected_month.strftime("%B")} {selected_month.year}')
@@ -281,21 +311,100 @@ with tab_month:
             'Μέγιστη': all_max
         })
     min_max_mean = min_max_mean.sort_values(by=['Ημέρα'])
+    st.write("Θερμοκρασία")
     chart_data = pd.DataFrame(min_max_mean, columns=["Ημέρα", "Ελάχιστη", "Μέση", "Μέγιστη"])
     st.line_chart(chart_data, x="Ημέρα", y=["Ελάχιστη", "Μέση", "Μέγιστη"])
-    st.subheader(f'{month}/{year}', divider='rainbow')
+
+    all_p_max = list()
+    all_p_min = list()
+    all_p_mean = list()
+    for i in pd.unique(filtered_df_p_month["Ημέρα του μήνα"]):
+        # st.write(filtered_df_month)
+        # st.write(filtered_df_month["Ημέρα του μήνα"] == f'{i}')
+        current_values = filtered_df_p_month[filtered_df_p_month["Ημέρα του μήνα"] == f'{i}']
+        current_max = current_values["Πίεση"].max()
+        all_p_max.append(current_max)
+        current_min = current_values["Πίεση"].min()
+        all_p_min.append(current_min)
+        current_mean = round(current_values["Πίεση"].mean(), 1)
+        all_p_mean.append(current_mean)
+
+    if len(all_p_min) > 0:
+        min_max_mean_p = pd.DataFrame(
+            data={
+                'Ημέρα': pd.unique(filtered_df_p_month["Ημέρα του μήνα"]),
+                'Ελάχιστη': all_p_min,
+                'Μέση': all_p_mean,
+                'Μέγιστη': all_p_max
+            })
+        min_max_mean_p = min_max_mean_p.sort_values(by=['Ημέρα'])
+        # chart_data = pd.DataFrame(min_max_mean_p, columns=["Ημέρα", "Ελάχιστη", "Μέση", "Μέγιστη"])
+        # st.line_chart(chart_data, x="Ημέρα", y=["Ελάχιστη", "Μέση", "Μέγιστη"])
+
+        min_val = min(all_p_min)
+        max_val = max(all_p_max)
+        c = (
+            alt.Chart(min_max_mean_p)
+            .mark_line()
+            .encode(alt.Y("Ελάχιστη").scale(domain=(min_val, max_val)), x="Ημέρα", color=alt.value("#d61a1f"))
+        )
+        d = (
+            alt.Chart(min_max_mean_p)
+            .mark_line()
+            .encode(alt.Y("Μέγιστη").scale(domain=(min_val, max_val)), x="Ημέρα", color=alt.value("#5453c6"))
+        )
+        e = (
+            alt.Chart(min_max_mean_p)
+            .mark_line()
+            .encode(alt.Y("Μέση").scale(domain=(min_val, max_val)), x="Ημέρα", color=alt.value("#f9c31a"))
+        )
+        st.write("Ατμοσφαιρική Πίεση")
+        st.altair_chart(c + d + e, use_container_width=True)
+
+    all_h_max = list()
+    all_h_min = list()
+    all_h_mean = list()
+    for i in pd.unique(filtered_df_h_month["Ημέρα του μήνα"]):
+        # st.write(filtered_df_month)
+        # st.write(filtered_df_month["Ημέρα του μήνα"] == f'{i}')
+        current_values = filtered_df_h_month[filtered_df_h_month["Ημέρα του μήνα"] == f'{i}']
+        current_max = current_values["Υγρασία"].max()
+        all_h_max.append(current_max)
+        current_min = current_values["Υγρασία"].min()
+        all_h_min.append(current_min)
+        current_mean = round(current_values["Υγρασία"].mean(), 1)
+        all_h_mean.append(current_mean)
+
+    if len(all_h_min) > 0:
+        min_max_mean_h = pd.DataFrame(
+            data={
+                'Ημέρα': pd.unique(filtered_df_h_month["Ημέρα του μήνα"]),
+                'Ελάχιστη': all_h_min,
+                'Μέση': all_h_mean,
+                'Μέγιστη': all_h_max
+            })
+        min_max_mean_h = min_max_mean_h.sort_values(by=['Ημέρα'])
+        st.write("Υγρασία")
+        chart_data = pd.DataFrame(min_max_mean_h, columns=["Ημέρα", "Ελάχιστη", "Μέση", "Μέγιστη"])
+        st.line_chart(chart_data, x="Ημέρα", y=["Ελάχιστη", "Μέση", "Μέγιστη"])
+
+    st.subheader(f'{month}/{year} | Θερμοκρασία', divider='rainbow')
     st.dataframe(min_max_mean, column_order=["Ημέρα", "Ελάχιστη", "Μέση", "Μέγιστη"], hide_index=True, use_container_width=True)
+    if len(all_p_min) > 0:
+        st.subheader(f'{month}/{year} | Ατμοσφαιρική Πίεση', divider='rainbow')
+        st.dataframe(min_max_mean_p, column_order=["Ημέρα", "Ελάχιστη", "Μέση", "Μέγιστη"], hide_index=True,
+                     use_container_width=True)
+    if len(all_h_min) > 0:
+        st.subheader(f'{month}/{year} | Υγρασία', divider='rainbow')
+        st.dataframe(min_max_mean_h, column_order=["Ημέρα", "Ελάχιστη", "Μέση", "Μέγιστη"], hide_index=True,
+                     use_container_width=True)
 
 with tab_year:
-    # if 'yyear' in st.session_state:
-    #     yyear = st.session_state['yyear']
-    #     selected_year = datetime.now().replace(year=yyear)
-    # else:
-    #     selected_year = datetime.now()
-    #     st.session_state['yyear'] = datetime.now().year
     option = st.selectbox("Επιλογή έτους", pd.unique(df["Έτος"]))
     st.write("Επιλεγμένο έτος: ", option)
     filtered_df_year = df.loc[df["Έτος"] == f'{option}']
+    filtered_df_year_p = df_pressure.loc[df_pressure["Έτος"] == f'{option}']
+    filtered_df_year_h = df_humidity.loc[df_humidity["Έτος"] == f'{option}']
     for i in range(12):
         filtered_df_year_per_month = df.loc[df["Μήν/Έτος"] == f'{i+1}/{option}']
         if filtered_df_year_per_month.size > 0:
@@ -306,8 +415,21 @@ with tab_year:
         x='Μήν/Έτος',
         y='Θερμοκρασία'
     )
+
+    min_val = filtered_df_year_p.loc[filtered_df_year_p["Πίεση"].idxmin()]["Πίεση"]
+    max_val = filtered_df_year_p.loc[filtered_df_year_p["Πίεση"].idxmax()]["Πίεση"]
+    chart_p = alt.Chart(filtered_df_year_p).mark_boxplot(extent='min-max').encode(
+        alt.Y("Πίεση").scale(domain=(min_val, max_val)),
+        x='Μήν/Έτος'
+    )
+    chart_h = alt.Chart(filtered_df_year_h).mark_boxplot(extent='min-max').encode(
+        x='Μήν/Έτος',
+        y='Υγρασία'
+    )
     st.subheader(f'{option}', divider='rainbow')
     st.altair_chart(chart, theme="streamlit", use_container_width=True)
+    st.altair_chart(chart_p, theme="streamlit", use_container_width=True)
+    st.altair_chart(chart_h, theme="streamlit", use_container_width=True)
 
 # st.write(data)
 
